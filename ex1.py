24cd2c58d6da81e09a508216717358ec4db90035
Fabricio <chubyxd1627@gmail.com>
2025-02-17 19:53:02 -0400

    puse f nomas maestro :(

--- ex1.py
+++ ex1.py
@@ -3,5 +3,5 @@ n = int(input("Type the number of elements you want to sum"))
 for i in range(n):
     num = float(input(f"Enter number {i+1}: "))
     res += num
-
+print("fnomas maestro")
 print(f"the sum is: {res}, is it clear motherfuck#r?")
