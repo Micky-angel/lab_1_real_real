191dc14b56bad96c3b45fcf4f50900188db71e62
miguelSilva <miguel.silva@ucb.edu.bo>
2025-02-17 14:44:17 -0400

    codigo1 agregado

--- ex1.py
+++ ex1.py
@@ -1,1 +1,7 @@
-print("hello world")
+res = 0
+n = int(input("Type the number of elements you want to sum"))
+for i in range(n):
+    num = float(input(f"Enter number {i+1}: "))
+    res += num
+
+print(f"the sum is: {res}")
